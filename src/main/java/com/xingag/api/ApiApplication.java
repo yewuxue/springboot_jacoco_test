55437eba7981e33564aa0ad54cf0ff6c2a8179ae
yangshuang <yangshuang@huimei.com>
2021-02-09 14:12:43 +0800

    Default Changelist

---
+++ src/main/java/com/xingag/api/ApiApplication.java
@@ -0,0 +1,44 @@
+package com.xingag.api;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.context.annotation.Bean;
+
+/***
+ * 入口文件
+ *
+ * 欢迎关注公众号：AirPython
+ */
+
+//@SpringBootApplication
+//public class ApiApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ApiApplication.class, args);
+//    }
+//
+//    @Bean
+//    public TomcatServletWebServerFactory servletContainer(){
+//        return new TomcatServletWebServerFactory(8081) ;
+//    }
+//}
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ApiApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiApplication.class);
+    }
+}
